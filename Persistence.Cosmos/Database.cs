dd1c85c58d6ad3b168b9914a2c672cf49f943013
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-05-13 14:16:21 +0100

    _

---
+++ Persistence.Cosmos/Database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Azure.Cosmos;
+
+namespace Sensemaking.Persistence.Cosmos
+{
+    public static class Database
+    {
+        private static string? EndPoint;
+        private static string? AuthorisationKey;
+        internal static string? DatabaseName;
+
+        private static readonly Lazy<CosmosClient> Client = new Lazy<CosmosClient>(() => 
+            new CosmosClient(EndPoint, AuthorisationKey, new CosmosClientOptions { Serializer = new Serializer() }), 
+            LazyThreadSafetyMode.PublicationOnly);
+
+        public static void Configure(string endPoint, string authenticationKey, string databaseName)
+        {
+            EndPoint = endPoint;
+            AuthorisationKey = authenticationKey;
+            DatabaseName = databaseName;
+        }
+
+        internal static CosmosClient GetClient()
+        {
+            if (!IsConfigured)
+                throw new Exception("Please provide end point, authorisation key and database name");
+
+            return Client.Value;
+        }
+
+        private static bool IsConfigured => !string.IsNullOrEmpty(EndPoint) && !string.IsNullOrEmpty(AuthorisationKey) && !string.IsNullOrEmpty(DatabaseName);
+    }
+}
---
+++ Persistence.Cosmos/Serializer.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Serialization;
+using System.Text;
+using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+
+namespace Sensemaking.Persistence.Cosmos
+{
+    internal class Serializer : CosmosSerializer
+    {
+        private static readonly Encoding DefaultEncoding = new UTF8Encoding(false, true);
+        private readonly JsonSerializer serializer;
+        private readonly JsonSerializerSettings serializerSettings;
+
+        public Serializer()
+        {
+            this.serializerSettings = Serialization.GetSettings();
+            this.serializer = JsonSerializer.Create(this.serializerSettings);
+        }
+
+        public override T FromStream<T>(Stream stream)
+        {
+            using (stream)
+            {
+                if (typeof(Stream).IsAssignableFrom(typeof(T)))
+                    return (T)(object)(stream);
+
+                using (var sr = new StreamReader(stream))
+                {
+                    using (var jsonTextReader = new JsonTextReader(sr))
+                        return serializer.Deserialize<T>(jsonTextReader)!;
+                }
+            }
+        }
+
+        public override Stream ToStream<T>(T input)
+        {
+            var streamPayload = new MemoryStream();
+            using (var streamWriter = new StreamWriter(streamPayload, encoding: DefaultEncoding, bufferSize: 1024, leaveOpen: true))
+            {
+                using (var writer = new JsonTextWriter(streamWriter))
+                {
+                    writer.Formatting = Formatting.None;
+                    serializer.Serialize(writer, input);
+                    writer.Flush();
+                    streamWriter.Flush();
+                }
+            }
+
+            streamPayload.Position = 0;
+            return streamPayload;
+        }
+    }
+}
