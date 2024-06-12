f0852550a1021b733d0406be9a34c543a39ffd9e
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2024-06-12 14:00:40 +0100

    _

--- Query/IQuery.cs
+++ Query/IQuery.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Sensemaking.Query
+namespace Sensemaking.Query;
+
+public interface IQuery<in T, U>
 {
-    public interface IQuery<in T, U>
-    {
-        Task<IEnumerable<U>> GetResultsAsync(T parameters);
-    }
+    Task<IEnumerable<U>> GetResultsAsync(T parameters);
+}
+
+public interface IRunQueries
+{
+    Task<IEnumerable<T>> GetResults<T>(string container, string query);
+    Task<IEnumerable<T>> GetResults<T>((string Container, string Query) definition);
 }
